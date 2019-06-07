28b5a10a3fc57ba352d9bf7ca4cb9a9e0d504f69
Guillaume de Matos guillaume.de-matos@epitech.eu <guillaume.de-matos@epitech.eu>
2019-06-07 16:27:42 +0200

    Add useMouse, useScroll and useSize

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 import { useEffect } from 'react';
 import { watchViewport, unwatchViewport } from 'tornis';
 
-export default (updateCallback) => {
-  useEffect(() => {
-    watchViewport(updateCallback);
+const depHasChanged = (deps, values) => deps.some(dep => values[dep] && values[dep].changed);
 
-    return () => unwatchViewport(updateCallback);
+export const useTornis = (updateCallback, dependencies = []) => {
+  const callback = (values) => {
+    if (dependencies.length === 0 || depHasChanged(dependencies, values)) {
+      updateCallback(values);
+    }
+  };
+
+  useEffect(() => {
+    watchViewport(callback);
+
+    return () => unwatchViewport(callback);
   });
 };
+
+const compose = (property, updateCallback) => {
+  const callback = (values) => {
+    updateCallback(values[property]);
+  };
+
+  useTornis(callback);
+};
+
+export const useSize = updateCallback => compose(
+  'size',
+  updateCallback,
+);
+
+export const useScroll = updateCallback => compose(
+  'scroll',
+  updateCallback,
+);
+
+export const useMouse = updateCallback => compose(
+  'mouse',
+  updateCallback,
+);
+
+export const useOrientation = updateCallback => compose(
+  'orientation',
+  updateCallback,
+);
+
+export default useTornis;
